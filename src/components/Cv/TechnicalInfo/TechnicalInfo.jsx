f3f0f454b40421781dcef7e4fb327d59513b4739
Thomas <thomasmetias29@gmail.com>
2025-08-18 19:18:12 +0300

    chore: structure all project's files and folders

---
+++ src/components/Cv/TechnicalInfo/TechnicalInfo.jsx
@@ -0,0 +1,12 @@
+import Profile from './Profile'
+import Experience from '../Experience'
+import Education from '../Education'
+import Portofolio from '../Portofolio'
+
+export default function TechnicalInfo() {
+  return (
+    <>
+
+    </>
+  )
+}
