bd1975d83c67e08235b0b97b03bb1af99746d746
Thomas <thomasmetias29@gmail.com>
2025-08-18 22:26:32 +0300

    feat: add page header with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import generatorLogo from "./assets/cv-generator-logo-lq.png"
+import Cv from "./components/Cv/Cv.jsx"
+import Form from "./components/Form/Form.jsx"
+
 export default function App() {
   return(
     <>
+      <header className="generator-header">
+          <img src={generatorLogo} alt="cv genenerator logo" className="generator-header__img" />
+          <span className="generator-header__text">CV Generator</span>
+      </header>
+      <main className="main-content">
+        <Cv />
+        <Form />
+      </main>
     </>
   )
 }
