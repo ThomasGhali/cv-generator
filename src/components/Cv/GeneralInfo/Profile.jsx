5fe0fb41e329a3d006581cce3978bb757bee1f96
Thomas <thomasmetias29@gmail.com>
2025-08-19 13:53:22 +0300

    feat!: add icon assets and make headers component
    - Add all assets needed as in icons needed.
    - make a SectionsHeader component to be used repeatidly
    without duplicating code (including its styles).

--- src/components/Cv/GeneralInfo/Profile.jsx
+++ src/components/Cv/GeneralInfo/Profile.jsx
@@ -1,6 +1,25 @@
+import "./Profile.css"
+import SectionsHeader from "../SectionsHeader"
+
 export default function Profile() {
   return (
     <>
+      <div className="profile-wrapper">
+        <section className="profile">
+          <div className="profile__img"></div>
+          <div className="profile__name-title">
+            <span className="profile__name">
+              Nina Lnade
+            </span>
+            <span>
+              Graphic Designer
+            </span>
+          </div>
+          
+        <SectionsHeader text="Contact" iconName="contact" />
+
+        </section>
+      </div>
     </>
   )
 }
---
+++ src/components/Cv/SectionsHeader.jsx
@@ -0,0 +1,33 @@
+import './SectionsHeader.css'
+import contact from '../../assets/contact.png'
+import skills from '../../assets/skills.png'
+import interests from '../../assets/interests.png'
+import profile from '../../assets/profile.png'
+import experience from '../../assets/experience.png'
+import education from '../../assets/education.png'
+import portofolio from '../../assets/portofolio.png'
+
+const icons = {
+  contact,
+  skills,
+  interests,
+  profile,
+  experience,
+  education,
+  portofolio,
+};
+// in the future it should be passed th icon name
+export default function SectionsHeader({ iconName, text }) {
+
+  return (
+    <>
+      <div className="header">
+        <img src={icons[iconName]} className='header__icon' />
+        <div className="header__info">
+          <div className="header__name">{text}</div>
+          <div className="header__line"></div>
+        </div>
+      </div>
+    </>
+  )
+}
