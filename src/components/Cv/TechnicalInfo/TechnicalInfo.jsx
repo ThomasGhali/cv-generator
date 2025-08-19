280cafcce3cd82aafe778127a547017d96ecbec8
Thomas <thomasmetias29@gmail.com>
2025-08-19 17:44:27 +0300

    feat: add the profile section with styles

--- src/components/Cv/TechnicalInfo/TechnicalInfo.jsx
+++ src/components/Cv/TechnicalInfo/TechnicalInfo.jsx
@@ -8,6 +8,7 @@ export default function TechnicalInfo() {
         <SectionsHeader text="Profile" iconName="profile" />
         <p>Passionate web developer with hands-on experience in building responsive and user-friendly websites using modern technologies. Skilled in HTML, CSS, JavaScript, and React, with a strong eye for detail and clean code practices.</p>
       </section>
+      
     </div>
   )
 }
