4c773ac98be59e16a61956ee5e9ce42bee7ddad3
Thomas <thomasmetias29@gmail.com>
2025-08-21 12:35:22 +0300

    fix: code issue caused rendering problems

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -155,230 +155,229 @@ export default function Form({
   return (
     <>
       <form action="">
-        <FormSection
-          name="Contact"
-        >
-          <label htmlFor="email">Email </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            onChange={setEmail}
-          />
+        <fieldset>
+          <legend>General Info</legend>
+            <FormSection
+              name="Contact"
+            >
+              <label htmlFor="email">Email </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="phone">Phone </label>
+              <input
+                type="tel"
+                placeholder="+1 234 567 890"
+                name="phone"
+                id="phone"
+                onChange={(e) => setPhone(e.target.value)}
+              />
+              <label htmlFor="website">Website </label>
+              <input
+                type="url"
+                name="website"
+                id="website"
+                onChange={(e) => setWebsite(e.target.value)}
+              />
+            </FormSection>
+            <FormSection
+              name="Skills"
+            >
+              {viewSkills()}
+              <button className='down-margin' type="button" onClick={handleSkillBtn}>Add skill</button>
+              <button type="button" onClick={() => setSkills([])}>Reset Skills</button>
+            </FormSection>
+            <FormSection
+              name="Interests"
+            >
+              <label htmlFor="interests">Interests</label>
+              <input
+                type="text"
+                name="interests"
+                id="interests"
+                placeholder='e.g. Minimalist Art'
+                value={interestInput}
+                onChange={(e) => setInterestInput(e.target.value)}
+              />
+              <button type="button" className='down-margin' onClick={handleAddInterestsBtn}>Add an interest</button>
+              <button type="button" onClick={handleResetInterestsBtn}>Reset interests</button>
+            </FormSection>
+        </fieldset>
 
-          <label htmlFor="phone">Phone </label>
-          <input
-            type="tel"
-            placeholder="+1 234 567 890"
-            name="phone"
-            id="phone"
-            onChange={setPhone}
-          />
-          <label htmlFor="website">Website </label>
-          <input
-            type="url"
-            name="website"
-            id="website"
-            onChange={setWebsite}
-          />
-
-        </FormSection>
-
-        <FormSection
-          name="Skills"
-        >
-          {viewSkills()}
-          <button className='down-margin' type="button" onClick={handleSkillBtn}>Add skill</button>
-          <button type="button" onClick={() => setSkills([])}>Reset Skills</button>
-        </FormSection>
-
-        <FormSection
-          name="Interests"
-        >
-          <label htmlFor="interests">Interests</label>
-          <input
-            type="text"
-            name="interests"
-            id="interests"
-            placeholder='e.g. Minimalist Art'
-            value={interestInput}
-            onChange={setInterestInput}
-          />
-          <button type="button" className='down-margin' onClick={handleAddInterestsBtn}>Add an interest</button>
-          <button type="button" onClick={handleResetInterestsBtn}>Reset interests</button>
-        </FormSection>
-
-        <FormSection
-          name="Profile"
-        >
-          <textarea onChange={setProfileDescription}></textarea>
-          <p className='tiny-note'>(Saved automatically)</p>
-        </FormSection>
-
-        <FormSection
-          name="Experience"
-        >
-          <label>
-            Position
-            <input type="text" 
-              name="position"
-              value={experience[experience.length - 1].position}
-              placeholder='e.g. Senior Front-End Developer'
-              onChange={handleExperience}
-            />
-          </label>        
-          <label>
-            Company
-            <input 
-              type="text"
-              name='company'
-              value={experience[experience.length - 1].company}
-              onChange={handleExperience}
-            />
-          </label>          
-          <label>
-            Location
-            <input 
-              type="text" 
-              name='location'
-              value={experience[experience.length - 1].location}
-              onChange={handleExperience}
-              placeholder='e.g. New York, NY'
-            />
-          </label>          
-          <label>
-            From
-            <input 
-              type="month"
-              name='from'
-              value={experience[experience.length - 1].from}
-              onChange={handleExperience}
-            />
-          </label>
-          <label>
-            To
-            <input 
-              type="month"
-              name='to'
-              value={experience[experience.length - 1].to}
-              onChange={handleExperience}
-            />
-          </label>
-          <label>
-            Experience More Info.
-            <textarea 
-              name="text" 
-              value={experience[experience.length - 1].text}
-              onChange={handleExperience}
-            />
-          </label>
-          <p className='tiny-note'>(all changes are dynamically saved)</p>       
-          <button 
-            className='down-margin' 
-            type='button'
-            onClick={handleAddExperienceBtn}
+        <fieldset>
+          <legend>Technical Info</legend>
+          <FormSection
+            name="Profile"
           >
-            Add Blank Experience
-          </button>
-          <button 
-            type="button"
-            onClick={handleResetExperienceBtn}
+            <textarea onChange={(e) => setProfileDescription(e.target.value)}></textarea>
+            <p className='tiny-note'>(Saved automatically)</p>
+          </FormSection>
+          <FormSection
+            name="Experience"
           >
-            Reset All Experiences
-          </button>
-        </FormSection>
-
-        <FormSection
-          name="Education"
-        >
-          <label>
-            Degree
-            <input type="text"
-              name="degree"
-              value={education.degree}
-              placeholder='e.g. Bachelor of Arts'
-              onChange={handleEducation}
-            />
-          </label>
-          <label>
-            Institution
-            <input 
-              type="text"
-              name='institution'
-              value={education.institution}
-              onChange={handleEducation}
-            />
-          </label>
-          <label>
-            Location
-            <input 
-              type="text" 
-              name='location'
-              value={education.location}
-              onChange={handleEducation}
-              placeholder='e.g. New York, NY'
-            />
-          </label>
-          <label>
-            Graduation Date
-            <input 
-              type="month"
-              name='gradYear'
-              value={education.gradYear}
-              onChange={handleEducation}
-            />
-          </label>
-        </FormSection>
-
-        <FormSection
-          name="Portofolio"
-          demo={true}
-        >
-          <label>
-            Project Name
-            <input type="text"
-              name="projectName"
-              value={projects[projects.length - 1].projectName}
-              placeholder='e.g. Social Media Campaign'
-              onChange={handleProjects}
-            />
-          </label>
-          <label>
-            For
-            <input 
-              type="text"
-              name='projectFor'
-              value={projects[projects.length - 1].projectFor}
-              onChange={handleProjects}
-            />
-          </label>
-          <label>
-            Info
-            <textarea 
-              name="projectText" 
-              value={projects[projects.length - 1].projectText}
-              onChange={handleProjects}
-            />
-          </label>
-          <p className='tiny-note'>(all changes are dynamically saved)</p>       
-          <button 
-            className='down-margin' 
-            type='button'
-            onClick={handleAddProjectBtn}
+            <label>
+              Position
+              <input type="text"
+                name="position"
+                value={experience[experience.length - 1].position}
+                placeholder='e.g. Senior Front-End Developer'
+                onChange={handleExperience}
+              />
+            </label>
+            <label>
+              Company
+              <input
+                type="text"
+                name='company'
+                value={experience[experience.length - 1].company}
+                onChange={handleExperience}
+              />
+            </label>
+            <label>
+              Location
+              <input
+                type="text"
+                name='location'
+                value={experience[experience.length - 1].location}
+                onChange={handleExperience}
+                placeholder='e.g. New York, NY'
+              />
+            </label>
+            <label>
+              From
+              <input
+                type="month"
+                name='from'
+                value={experience[experience.length - 1].from}
+                onChange={handleExperience}
+              />
+            </label>
+            <label>
+              To
+              <input
+                type="month"
+                name='to'
+                value={experience[experience.length - 1].to}
+                onChange={handleExperience}
+              />
+            </label>
+            <label>
+              Experience More Info.
+              <textarea
+                name="text"
+                value={experience[experience.length - 1].text}
+                onChange={handleExperience}
+              />
+            </label>
+            <p className='tiny-note'>(all changes are dynamically saved)</p>
+            <button
+              className='down-margin'
+              type='button'
+              onClick={handleAddExperienceBtn}
+            >
+              Add Blank Experience
+            </button>
+            <button
+              type="button"
+              onClick={handleResetExperienceBtn}
+            >
+              Reset All Experiences
+            </button>
+          </FormSection>
+          <FormSection
+            name="Education"
           >
-            Add Blank Project
-          </button>
-          <button 
-            type="button"
-            onClick={handleResetProjectBtn}
+            <label>
+              Degree
+              <input type="text"
+                name="degree"
+                value={education.degree}
+                placeholder='e.g. Bachelor of Arts'
+                onChange={handleEducation}
+              />
+            </label>
+            <label>
+              Institution
+              <input
+                type="text"
+                name='institution'
+                value={education.institution}
+                onChange={handleEducation}
+              />
+            </label>
+            <label>
+              Location
+              <input
+                type="text"
+                name='location'
+                value={education.location}
+                onChange={handleEducation}
+                placeholder='e.g. New York, NY'
+              />
+            </label>
+            <label>
+              Graduation Date
+              <input
+                type="month"
+                name='gradYear'
+                value={education.gradYear}
+                onChange={handleEducation}
+              />
+            </label>
+          </FormSection>
+          <FormSection
+            name="Portofolio"
+            demo={true}
           >
-            Reset All Projects
-          </button>
-
-        </FormSection>
+            <label>
+              Project Name
+              <input type="text"
+                name="projectName"
+                value={projects[projects.length - 1].projectName}
+                placeholder='e.g. Social Media Campaign'
+                onChange={handleProjects}
+              />
+            </label>
+            <label>
+              For
+              <input
+                type="text"
+                name='projectFor'
+                value={projects[projects.length - 1].projectFor}
+                onChange={handleProjects}
+              />
+            </label>
+            <label>
+              Info
+              <textarea
+                name="projectText"
+                value={projects[projects.length - 1].projectText}
+                onChange={handleProjects}
+              />
+            </label>
+            <p className='tiny-note'>(all changes are dynamically saved)</p>
+            <button
+              className='down-margin'
+              type='button'
+              onClick={handleAddProjectBtn}
+            >
+              Add Blank Project
+            </button>
+            <button
+              type="button"
+              onClick={handleResetProjectBtn}
+            >
+              Reset All Projects
+            </button>
+          </FormSection>
+        </fieldset>
       </form>
     </>
   )
 }
+
 
 
